34210071d2a494a12f4dc389dd7bbd1ea93059c0
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-07 12:30:26 +0400

    added depended components

--- src/WebComponent.php
+++ src/WebComponent.php
@@ -3,11 +3,14 @@
 namespace src;
 
 use Henrik\Contracts\BaseComponent;
+use Henrik\Contracts\ComponentInterfaces\DependsOnAwareInterface;
 use Henrik\Contracts\ComponentInterfaces\EventSubscriberAwareInterface;
 use Henrik\Contracts\EventDispatcherInterface;
+use Henrik\Session\SessionComponent;
+use Hk\Core\CoreComponent;
 use Hk\Web\Core\Subscribers\MatchedRouteSubscriber;
 
-class WebComponent extends BaseComponent implements EventSubscriberAwareInterface
+class WebComponent extends BaseComponent implements EventSubscriberAwareInterface, DependsOnAwareInterface
 {
     /**
      * {@inheritDoc}
@@ -16,6 +19,14 @@ class WebComponent extends BaseComponent implements EventSubscriberAwareInterfac
     {
         return [
             EventDispatcherInterface::class => [MatchedRouteSubscriber::class],
+        ];
+    }
+
+    public function dependsOn(): array
+    {
+        return [
+            CoreComponent::class,
+            SessionComponent::class
         ];
     }
 }
