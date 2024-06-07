908b26d27e643de2374598bb0b2729a11d8a28aa
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-07 17:29:07 +0400

    fixed bug

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$webComponent = new \Henrik\Web\WebComponent();
+
+var_dump($webComponent->getServices());
--- src/WebComponent.php
+++ src/WebComponent.php
@@ -7,10 +7,16 @@ use Henrik\Contracts\ComponentInterfaces\DependsOnAwareInterface;
 use Henrik\Contracts\ComponentInterfaces\EventSubscriberAwareInterface;
 use Henrik\Contracts\ComponentInterfaces\OnBootstrapAwareInterface;
 use Henrik\Contracts\CoreEvents;
+use Henrik\Contracts\Enums\ServiceScope;
 use Henrik\Contracts\EventDispatcherInterface;
 use Henrik\Contracts\Http\RequestInterface;
+use Henrik\Contracts\Utils\MarkersInterface;
 use Henrik\Core\CoreComponent;
+use Henrik\Events\EventDispatcher;
+use Henrik\Http\Request;
 use Henrik\Route\RouteComponent;
+use Henrik\View\Extension\AssetExtension;
+use Henrik\View\Renderer;
 use Henrik\Web\Subscribers\MatchedRouteSubscriber;
 use Henrik\Web\Subscribers\RequestHandlerSubscriber;
 
@@ -30,7 +36,36 @@ class WebComponent extends BaseComponent implements EventSubscriberAwareInterfac
 
     public function getServices(): array
     {
-        return require 'config/services.php';
+        return [
+            ServiceScope::SINGLETON->value => [
+                [
+                    'id'    => AssetExtension::class,
+                    'class' => AssetExtension::class,
+                    'args'  => [
+                        'basePath' => MarkersInterface::AS_SERVICE_PARAM_MARKER . 'assetsDir',
+                    ],
+                ],
+                [
+                    'id'     => Renderer::class,
+                    'class'  => Renderer::class,
+                    'params' => [
+                        'viewDirectory' => MarkersInterface::AS_SERVICE_PARAM_MARKER . 'viewDirectory',
+                    ],
+                ],
+                [
+                    'id'    => MatchedRouteSubscriber::class,
+                    'class' => MatchedRouteSubscriber::class,
+                ],
+                [
+                    'id'    => CoreEvents::ROUTE_DISPATCHER_DEFAULT_DEFINITION_ID,
+                    'class' => EventDispatcher::class,
+                ],
+            ],
+
+            ServiceScope::PARAM->value => [
+                RequestInterface::class => Request::createFromGlobals(),
+            ],
+        ];
     }
 
     /**
