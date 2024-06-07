96ba595f756dafc46d1166620b2aadb1f83bbadc
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-07 16:32:40 +0400

    fixed subscriber bug

--- src/WebComponent.php
+++ src/WebComponent.php
@@ -3,29 +3,27 @@
 namespace Henrik\Web;
 
 use Henrik\Contracts\ComponentInterfaces\DependsOnAwareInterface;
-use Henrik\Contracts\ComponentInterfaces\EventSubscriberAwareInterface;
+use Henrik\Contracts\ComponentInterfaces\OnBootstrapAwareInterface;
+use Henrik\Contracts\CoreEvents;
 use Henrik\Contracts\EventDispatcherInterface;
 use Henrik\Core\CoreComponent;
 use Henrik\Route\RouteComponent;
 use Henrik\Web\Subscribers\MatchedRouteSubscriber;
 
-class WebComponent extends CoreComponent implements EventSubscriberAwareInterface, DependsOnAwareInterface
+class WebComponent extends CoreComponent implements DependsOnAwareInterface, OnBootstrapAwareInterface
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getEventSubscribers(): array
-    {
-        return [
-            EventDispatcherInterface::class => [MatchedRouteSubscriber::class],
-        ];
-    }
-
     public function dependsOn(): array
     {
         return [
             CoreComponent::class,
             RouteComponent::class,
+        ];
+    }
+
+    public function onBootstrapDispatchEvents(): array
+    {
+        return [
+            EventDispatcherInterface::class => [MatchedRouteSubscriber::class => CoreEvents::HTTP_REQUEST_EVENTS],
         ];
     }
 }
